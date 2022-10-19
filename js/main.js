5017609189dfe08b9ada6a0ee8d2e407ab78f6dc
Pablo Carballo <pscarballo@gmail.com>
2022-10-19 01:33:06 -0300

    cambie ruta de imagenes en array

--- js/main.js
+++ js/main.js
@@ -13,18 +13,18 @@ let precioCompra = 0;
 
 
 const productos = [
-    { id: "aj9", Tipo: "Zapatilla", Nombre: "Jordan 9", precio: 110, imagen: "../images/zapatillas/aj9.jpg" },
-    { id: "aj10", Tipo: "Zapatilla", Nombre: "Jordan 10", precio: 115, imagen: "../images/zapatillas/aj10.jpg" },
-    { id: "aj11", Tipo: "Zapatilla", Nombre: "Jordan 11", precio: 120, imagen: "../images/zapatillas/aj11.jpg" },
-    { id: "aj12", Tipo: "Zapatilla", Nombre: "Jordan 12", precio: 125, imagen: "../images/zapatillas/aj12.jpg" },
-    { id: "aj13", Tipo: "Zapatilla", Nombre: "Jordan 13", precio: 130, imagen: "../images/zapatillas/aj13.jpg" },
-    { id: "aj14", Tipo: "Zapatilla", Nombre: "Jordan 14", precio: 135, imagen: "../images/zapatillas/aj14.jpg" },
-    { id: "r1", Tipo: "Remera", Nombre: "Remera Roja", precio: 80, imagen: "../images/remeras/r1.jpg" },
-    { id: "r2", Tipo: "Remera", Nombre: "Remera Amarilla", precio: 82, imagen: "../images/remeras/r2.jpg" },
-    { id: "r3", Tipo: "Remera", Nombre: "Remera Azul", precio: 88, imagen: "../images/remeras/r3.jpg" },
-    { id: "acc1", Tipo: "Accesorio", Nombre: "Media", precio: 20, imagen: "../images/accesorios/media.jpg" },
-    { id: "acc2", Tipo: "Accesorio", Nombre: "Gorra", precio: 50, imagen: "../images/accesorios/gorra.jpg" },
-    { id: "acc3", Tipo: "Accesorio", Nombre: "Vincha", precio: 40, imagen: "../images/accesorios/vincha.jpg" },
+    { id: "aj9", Tipo: "Zapatilla", Nombre: "Jordan 9", precio: 110, imagen: "./images/zapatillas/aj9.jpg" },
+    { id: "aj10", Tipo: "Zapatilla", Nombre: "Jordan 10", precio: 115, imagen: "./images/zapatillas/aj10.jpg" },
+    { id: "aj11", Tipo: "Zapatilla", Nombre: "Jordan 11", precio: 120, imagen: "./images/zapatillas/aj11.jpg" },
+    { id: "aj12", Tipo: "Zapatilla", Nombre: "Jordan 12", precio: 125, imagen: "./images/zapatillas/aj12.jpg" },
+    { id: "aj13", Tipo: "Zapatilla", Nombre: "Jordan 13", precio: 130, imagen: "./images/zapatillas/aj13.jpg" },
+    { id: "aj14", Tipo: "Zapatilla", Nombre: "Jordan 14", precio: 135, imagen: "./images/zapatillas/aj14.jpg" },
+    { id: "r1", Tipo: "Remera", Nombre: "Remera Roja", precio: 80, imagen: "./images/remeras/r1.jpg" },
+    { id: "r2", Tipo: "Remera", Nombre: "Remera Amarilla", precio: 82, imagen: "./images/remeras/r2.jpg" },
+    { id: "r3", Tipo: "Remera", Nombre: "Remera Azul", precio: 88, imagen: "./images/remeras/r3.jpg" },
+    { id: "acc1", Tipo: "Accesorio", Nombre: "Media", precio: 20, imagen: "./images/accesorios/media.jpg" },
+    { id: "acc2", Tipo: "Accesorio", Nombre: "Gorra", precio: 50, imagen: "./images/accesorios/gorra.jpg" },
+    { id: "acc3", Tipo: "Accesorio", Nombre: "Vincha", precio: 40, imagen: "./images/accesorios/vincha.jpg" },
 
 ];
 
